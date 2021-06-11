0c9d14d046730359e5684ba832f4906cef9bd875
Basith2410 <71713207+Basith2410@users.noreply.github.com>
2021-06-11 20:14:44 +0530

    Delete answer.py

--- task1/answer.py
+++
@@ -1,5 +0,0 @@
-import joblib
-experience = float(input('Enter your experience : '))
-model=joblib.load('salary_prediction_app.pk1')
-result=model.predict([[experience]])
-print(f'This is your predicted salary: {round(result[0])}')
