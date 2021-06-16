66137331858a80ea11082aabf81eecfdad0eecf4
Basith2410 <71713207+Basith2410@users.noreply.github.com>
2021-06-16 17:44:21 +0530

    Delete prediction_app.py

--- task1/prediction_app.py
+++
@@ -1,5 +0,0 @@
-import joblib
-experience = float(input('Enter your experience (in years) : '))
-model=joblib.load('salary_prediction_app.pk1')
-result=model.predict([[experience]])
-print(f'your monthly estimated salary is: {round(result[0])}')
